ac99e802c6989460e2285191bd9e278df08a64fd
Pauline-Oraro <hiltrapauline@gmail.com>
2025-08-04 17:16:29 +0300

    added the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
+import About from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () =>{
 
   return (
-    <div>
-      <h1 className="bg-red-500 text-3xl">cocktail craze</h1>
-    </div>
+    <main>
+      <Navbar/>
+      <Hero/>
+      <Cocktails/>
+      <About/>
+      <Art/>
+      <Menu/>
+      <Contact/>
+    </main>
   )
     
 }
